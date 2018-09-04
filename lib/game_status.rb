7a0fafe1fd679b3034ec543efeffe3acbc338142
carly <carly@noukod.com>
2018-09-04 15:32:23 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,15 +5,14 @@ end
 
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [
-  [0, 1, 2]
+[0, 1, 2]
 [3, 4, 5]
 [6, 7, 8]
 [0, 4, 8]
 [1, 4, 7]
 [0, 3, 6]
 [2, 5, 8]
-[2, 4, 6]
-]
+[2, 4, 6]]
 
 
 def won?(board)
